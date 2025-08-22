64566716b8ad0769b29bbdb8c3f371633008a30a
Philipp Reist <preist@nvidia.com>
2025-08-22 11:18:00 +0200

    Fix Anymal examples run instructions
    Signed-off-by: Philipp Reist <preist@nvidia.com>

--- newton/examples/example_anymal_c_walk.py
+++ newton/examples/example_anymal_c_walk.py
@@ -19,7 +19,7 @@
 # Shows how to control Anymal C with a pretrained policy.
 #
 # Example usage:
-# uv run --extra examples --extra torch-cu12 newton/examples/example_anymal_c_walk_on_sand.py
+# uv run --extra examples --extra torch-cu12 newton/examples/example_anymal_c_walk.py
 #
 ###########################################################################
 
--- newton/examples/example_anymal_c_walk_physx_policy.py
+++ newton/examples/example_anymal_c_walk_physx_policy.py
@@ -19,7 +19,7 @@
 # Shows how to control Anymal C with a policy pretrained in physx.
 #
 # Example usage:
-# uv run --extra examples --extra torch-cu12 newton/examples/example_anymal_c_walk_on_sand.py
+# uv run --extra examples --extra torch-cu12 newton/examples/example_anymal_c_walk_physx_policy.py
 #
 ###########################################################################
 
